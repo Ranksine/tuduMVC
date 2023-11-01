8a5bc2c8682c7815c73d8992f3d622ce7ec63eb6
_Arom <117594980+Ranksine@users.noreply.github.com>
2023-10-31 21:48:43 -0600

    Actualización de ver-pais
    Implementación de cambios para la seccion ver-pais.
    Consumo correcto de la api restcountries

---
+++ app/paises/ver-pais/controller/ver-pais.controller.js
@@ -0,0 +1,53 @@
+let code = window.location.pathname;
+let paisInfo = {};
+code = code.split('/');
+code = code[code.length - 1];
+
+console.log(code);
+traerInfo(code);
+
+
+
+async function traerInfo(code) {
+    let url = `https://restcountries.com/v3.1/alpha/${code}`;
+    let respuesta = await fetch(url);
+    
+    await respuesta.json().then(
+        (pais) => {
+            paisInfo = pais[0];
+            console.log(paisInfo);
+            renderInfo();
+        }
+    );
+}  
+
+function renderInfo() {
+
+    let flagImg = document.getElementById('flagImg');
+    flagImg.src = paisInfo.flags.svg;
+    console.log(flagImg.src);
+    flagImg.alt = paisInfo.flags.alt;
+
+    let divNameMain = document.getElementById('divNameMain');    
+    let bReg = document.getElementById('paisRegion');
+    let bCap = document.getElementById('paisCapital');
+    let bPob = document.getElementById('paisPoblacion');
+    let bLang = document.getElementById('paisLang');
+
+    let idiomasObj = paisInfo.languages;
+    let fstIdioma;
+    Object.entries(idiomasObj).forEach(([codigoIdioma, nombreIdioma]) => {
+        // Guarda el primer idioma y rompe el bucle
+        if (!fstIdioma) {
+          fstIdioma = nombreIdioma;
+        }
+      });
+
+    
+    divNameMain.innerText = `${paisInfo.flag} ${paisInfo.name.common}`;
+    bReg.innerText = `${paisInfo.region}`;
+    bCap.innerText = `${paisInfo.capital}`;
+    bPob.innerText = `${paisInfo.population}`;
+    console.log(fstIdioma);
+    bLang.innerText = `${fstIdioma}`;
+}
